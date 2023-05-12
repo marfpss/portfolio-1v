f9d0cd8844261ae62969bae5d60218a97ebc1ef3
marfpss <mbbrpiuoficial@gmail.com>
2023-05-12 02:29:47 -0300

    Up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,17 +7,20 @@ import { FaInstagram, FaGithub } from "react-icons/fa";
 const Home = () => {
   return (
     <Container id="container" className="container">
-      <Card id="card" className="d-flex flex-column align-items-center justify-content-center text-center">
-        <div className="avatar"> <img src="https://cdn.discordapp.com/attachments/901945232529162250/1106412807609667674/6db2e35013a4252696a9bb7c7804a701.jpg" alt="Avatar" /></div>
-        <h1 id="name">
-          Marcelo
-        </h1>
-        <h2 id="surname">
-          Mais conhecido como MarFpss
-        </h2>
-        <h3 id="about">
-          Sobre mim
-        </h3>
+      <Card
+        id="card"
+        className="d-flex flex-column align-items-center justify-content-center text-center"
+      >
+        <div className="avatar">
+          {" "}
+          <img
+            src="https://cdn.discordapp.com/attachments/901945232529162250/1106412807609667674/6db2e35013a4252696a9bb7c7804a701.jpg"
+            alt="Avatar"
+          />
+        </div>
+        <h1 id="name">Marcelo</h1>
+        <h2 id="surname">Mais conhecido como MarFpss</h2>
+        <h3 id="about">Sobre mim</h3>
         <p id="description">
           👋 Olá, meu nome é Marcelo e tenho 17 anos. Sou apaixonado por
           tecnologia e atualmente estou estudando 🌐 Front-End e 💻 programação.
@@ -39,10 +42,20 @@ const Home = () => {
           incríveis e fazer a diferença no mundo da tecnologia!
         </p>
         <div id="icon" className="d-flex justify-content-center">
-          <a id="icon" target="_blank" href="https://www.instagram.com/marcelokkk_019/" className="icon-link">
+          <a
+            id="icon"
+            target="_blank"
+            href="https://www.instagram.com/marcelokkk_019/"
+            className="icon-link"
+          >
             <FaInstagram className="icon" />
           </a>
-          <a id="icon" target="_blank" href="https://github.com/marfpss" className="icon-link">
+          <a
+            id="icon"
+            target="_blank"
+            href="https://github.com/marfpss"
+            className="icon-link"
+          >
             <FaGithub className="icon" />
           </a>
         </div>
--- src/components/MyProjectsGithubRepository.jsx
+++ src/components/MyProjectsGithubRepository.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/MyProjectsGithubRepository.css";
 import { Container, Card } from "react-bootstrap";
@@ -9,7 +9,9 @@ const MyProjectsGithubRepository = ({ username }) => {
 
   useEffect(() => {
     const fetchProjects = async () => {
-      const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+      const response = await axios.get(
+        `https://api.github.com/users/${username}/repos`
+      );
       setProjects(response.data);
     };
 
@@ -19,12 +21,22 @@ const MyProjectsGithubRepository = ({ username }) => {
   return (
     <Container id="container" className="container">
       <h1>Meus Projetos</h1>
-      <Card id="card" className="d-flex flex-column align-items-center justify-content-center text-center">
+      <Card
+        id="card"
+        className="d-flex flex-column align-items-center justify-content-center text-center"
+      >
         {projects.map((project) => (
           <div className="project" key={project.id}>
             <div className="project-name">{project.name}</div>
             <div className="project-description">{project.description}</div>
-            <a className="project-link" href={project.html_url} target="_blank" rel="noopener noreferrer">Link para o repositório do projeto</a>
+            <a
+              className="project-link"
+              href={project.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Link para o repositório do projeto
+            </a>
           </div>
         ))}
       </Card>

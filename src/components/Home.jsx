b0e21ed5de9d3d52a143b55ac180df12c9abb9f1
marfpss <mbbrpiuoficial@gmail.com>
2023-06-18 01:14:24 -0300

    Up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
+import { FaSun, FaMoon, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import './css/Home.css';
 
 const Home = () => {
   const [bio, setBio] = useState('');
   const [readme, setReadme] = useState('');
+  const [projects, setProjects] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     const fetchBio = async () => {
@@ -27,16 +30,55 @@ const Home = () => {
       }
     };
 
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get('https://api.github.com/users/marfpss/repos');
+        setProjects(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     fetchBio();
     fetchReadme();
+    fetchProjects();
   }, []);
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="container">
+    <div className={`container ${darkMode ? 'dark' : ''}`}>
       <img src="https://avatars.githubusercontent.com/marfpss" alt="Perfil" className="profile-image" />
-      <h1 className="name">Marcelo</h1>
-      <p className="description">{bio}</p>
-      <ReactMarkdown className="readme" children={readme} />
+      <h1 className="name">
+        Marcelo{' '}
+        <button className="toggle-button" onClick={toggleDarkMode}>
+          {darkMode ? <FaSun /> : <FaMoon />}
+        </button>
+      </h1>
+      <p className={`description ${darkMode ? 'dark' : ''}`}>{bio}</p>
+      <div className="social-icons">
+        <a href="https://www.instagram.com/marcelokkk_019" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="social-icon" />
+        </a>
+        {/* <a href="#" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="social-icon" />
+        </a> */}
+      </div>
+      <ReactMarkdown className={`readme ${darkMode ? 'dark' : ''}`} children={readme} />
+      <h2 className={`projects-title ${darkMode ? 'dark' : ''}`}>Meus Projetos</h2>
+      <div className="projects">
+        {projects.map((project) => (
+          <button
+            key={project.id}
+            className={`project-button ${darkMode ? 'dark' : ''}`}
+            onClick={() => window.open(project.html_url, '_blank')}
+          >
+            {project.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
